2b3bce891db9e1c80c16c6283dc19e305ee4280b
Plasnid <stuart@sackler.ca>
2020-03-24 00:07:28 -0400

    added notes

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
+// * holds onto the context we put our image data into
 let tempContext;
-let start;
 
+// *this function gets run when we get back processed data from the worker
 function changeImage(e){
     tempContext.putImageData(new ImageData(e.data,960), 0, 0);
-    let diff = new Date() - start;
-    log.innerText = `Process done in ${diff}ms with web workers`;
 }
 
 function process() {
+    // * identify the source and the canvas
     let source = document.getElementById("source");
-    start = new Date();
     let canvas = document.getElementById("target");
+
+    // *select the width and height of the canvas...we shrink this a bit in css
+    // *if the canvas width and height doesn't match the image width and height initially this breaks
     canvas.width = source.clientWidth;
     canvas.height = source.clientHeight;
 
+    // *setting the context of the canvas to 2d
     tempContext = canvas.getContext("2d");
-    // len is the number of items in the binaryData array
-    // it is 4 times the number of pixels in the canvas object
+    // *len is the number of items in the binaryData array
+    // *it is 4 times the number of pixels in the canvas object
     let len = canvas.width * canvas.height * 4;
-
+    // *here we draw the initial image to the canvas 
     tempContext.drawImage(source, 0, 0, canvas.width, canvas.height);
 
+    // *get the data from the canvas at that moment
     let canvasData = tempContext.getImageData(0, 0, canvas.width, canvas.height);
+    // *the canvas data is held in a big array  called data
     let binaryData = canvasData.data;
 
-    // processSepia is a variation of the previous version. See below
-    //processSepia(binaryData, len);
+    // *we define a new worker and tell it what script to run
+    // *surprise!  server side node likes var
     let w = new Worker('sepiaWorker.js');
+    // *when we get a message back from our script, we run changeImage
     w.onmessage = changeImage;
+    // *we send our worker the data we would like it to process
     w.postMessage({binDat:binaryData, howLong:len});
 };
 process();
--- sepiaWorker.js
+++ sepiaWorker.js
@@ -1,13 +1,16 @@
 function noise() {
-    //Returns a value between 0.5 and 1
+    // * Returns a value between 0.5 and 1
     return Math.random() * 0.5 + 0.5;
 };
 
 function colorDistance(scale, dest, src) {
-    // returns a red, blue or green value for the 'sepia' pixel
-    // which is a weighted average of the original value and the calculated value
+    // * returns a red, blue or green value for the 'sepia' pixel
+    // * which is a weighted average of the original value and the calculated value
     return (scale * dest + (1 - scale) * src);
 };
+
+// *process sepia workes with colour distance and noise to make our image sepia
+// *so much var, but let will break our worker!
 var processSepia = function (binaryData, l) {
     for (var i = 0; i < l; i += 4) {
         var r = binaryData[i];
@@ -18,9 +21,11 @@ var processSepia = function (binaryData, l) {
         binaryData[i + 1] = colorDistance(noise(), (r * 0.349) + (g * 0.686) + (b * 0.168), g);
         binaryData[i + 2] = colorDistance(noise(), (r * 0.272) + (g * 0.534) + (b * 0.131), b);
     }
+    // *finally we post back the data to the script the called the worker
     postMessage(binaryData);
 };
 
+// *when we get a message, we send the data and the length to processSepia
 self.onmessage = function(msg){
     this.processSepia(msg.data.binDat, msg.data.howLong);
 }
